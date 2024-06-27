7b893e203dbc4595b0263bc9f4d4841f4bbb52be
Aditya <4adi26@gmail.com>
2024-06-27 08:45:09 +0530

    create speed context

---
+++ src/context/SpeedContext.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, createContext, useState } from "react";
+import { SpeedType } from "../utils/types";
+
+export interface speedContextInterface {
+  speed: SpeedType;
+  setSpeed: (speed: SpeedType) => void;
+}
+
+export const SpeedContext = createContext<speedContextInterface | undefined>(undefined);
+
+export const SpeedProvider = ({ children }: { children: ReactNode }) => {
+  const [speed, setSpeed] = useState<SpeedType>(0.5)
+  return (
+    <SpeedContext.Provider value={{ speed, setSpeed }}>
+      {children}
+    </SpeedContext.Provider>
+  )
+}
